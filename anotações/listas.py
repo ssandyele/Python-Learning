655865cb3325b0a48d1b873d1b762b4119c7c50b
ssandyele <severosandyele@gmail.com>
2026-01-14 09:49:45 -0300

    Add list notes

---
+++ anotações/listas.py
@@ -0,0 +1,118 @@
+#LISTAS -----------------------------------------------------------
+
+# Podemos declará-las usando []
+# Exemplo: 
+
+num = [2, 5, 9, 1]
+
+print(num) 
+# [2, 5, 9, 1]
+num[2]=3 #isso funciona, pois listas são mutáveis 
+print(num)
+# [2, 5, 3, 1]
+num.append(7) #adiciona o número 7 no final, adicionando mais um índice
+print(num)
+# [2, 5, 3, 1, 7]
+
+num.sort() #coloca a lista em ordem crescente
+print(num)
+# [1, 2, 3, 5, 7]
+num.sort(reverse=True) #coloca a lista em ordem DEcrescente
+print(num)
+# [7, 5, 3, 2, 1]
+
+tam=len(num) #função que retorna quantos elementos a lista possui
+print(f'Essa lista tem {tam} elementos')
+
+num.insert(2, 0) #na posição 2, adiciona o número 0
+print(num)
+# [7, 5, 0, 3, 2, 1]
+
+num.pop() #deleta o elemento do último índice
+print(num)
+# [7, 5, 0, 3, 2]
+num.pop(2) #deleta o elemento do índice 2
+print(num)
+# [7, 5, 3, 2]
+
+num.insert(2, 2) #insere o número 2 no índice 2
+print(num)
+# [7, 5, 2, 3, 2]
+
+num.remove(7) #remove o elemento 7
+print(num)
+# [5, 2, 3, 2]
+num.remove(2) #remove o número 2 (como ele vai do início ao fim, procurando o 2, ele vai deletar apenas o primeiro que aparecer)
+print(num)
+# [5, 3, 2]
+#num.remove(4) #remove o elemento 4
+#print(num)
+# retorna um erro, pois não há '4' na lista
+if 4 in num: #verifica se o elemento 4 está na lista antes de removê-lo
+    num.remove(4)
+else:
+    print('O número 4 não foi encontrado na lista')
+
+
+#------------------------------------------------------------------
+# Iniciando uma lista vazia
+#1
+valores = []
+valores.append(5)
+valores.append(9)
+valores.append(4)
+
+print(valores)
+# [5, 9, 4]
+
+for v in valores:
+    print(f'{v}...', end =' ')
+# 5... 9... 4... 
+
+print('\n')
+
+for c, v in enumerate(valores):
+    print(f'Na posição {c}, encontrei o valor {v}!')
+print('Cheguei ao final da lista.')
+# Na posição 0, encontrei o valor 5!
+# Na posição 1, encontrei o valor 9!
+# Na posição 2, encontrei o valor 4!
+# Cheguei ao final da lista.
+
+#2
+elem = list()
+for c in range(0, 5):
+    elem.append(int(input('Digite um valor: '))) #preenche uma lista com valores digitados pelo usuário
+
+for pos, e in enumerate(elem):
+    print(f'Na posição {pos}, encontrei o valor {e}!')
+print('Cheguei ao final da lista.')
+#exemplo de saída: 
+# Na posição 0, encontrei o valor 5!
+# Na posição 1, encontrei o valor 6!
+# Na posição 2, encontrei o valor 1!
+# Na posição 3, encontrei o valor 8!
+# Na posição 4, encontrei o valor 3!
+# Cheguei ao final da lista.
+
+
+#------------------------------------------------------------------
+# Cópia e ligação de listas
+
+# ligação: se altero uma, a outra se altera também
+a = [2, 3, 4, 7]
+b = a #cria uma ligação entre as listas b & a
+b[2]=8 #o índice 2 da lista B recebe o elemento '8', mas por conta da ligação, a lista A também é alterada da mesma forma
+print(f'Lista A: {a}')
+print(f'Lista B: {b}')
+# Lista A: [2, 3, 8, 7]
+# Lista B: [2, 3, 8, 7]
+
+# cópia: apenas faz uma copia de uma lista
+c = [3, 5, 7, 9]
+d = c[:] # D recebe todos os valores de C (cópia de C dentro de D)
+d[2]=0 #altera o elemento do índice 2 da apenas da lista D, pois não há conexão entre C & D
+print(f'Lista C: {c}')
+print(f'Lista D: {d}')
+# Lista C: [3, 5, 7, 9]
+# Lista D: [3, 5, 0, 9]
